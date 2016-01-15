ce3d801ccd9c9dc940028e876fb78a8f2924a4be
Nick Hollon <falling.masonry@gmail.com>
2016-01-14 16:47:03 -0800

    Initial commit

---
+++ Main.elm
@@ -0,0 +1,13 @@
+module Main (..) where
+
+import Html exposing (..)
+import Html.Attributes exposing (href)
+
+
+main =
+    main'
+        []
+        [ h1 [] [ text "Nick Hollon solves problems" ]
+        , h2 [] [ text "Nick Hollon can solve <em>your</em> problems" ]
+        , h3 [] [ a [ href "mailto:contact@nickhollon.com" ] [ text "Get help today" ] ]
+        ]
