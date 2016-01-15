4b33f308040c8f02a3774e5de7fc20288e084010
Nick Hollon <falling.masonry@gmail.com>
2016-01-14 17:16:52 -0800

    Switch to Markdown

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,24 @@
 module Main (..) where
 
 import Html exposing (..)
-import Html.Attributes exposing (href)
+import Markdown
 
 
 main : Html
 main =
     main'
         []
-        [ h1 [] [ text "Nick Hollon solves problems" ]
-        , h2 [] [ text "Nick Hollon can solve <em>your</em> problems" ]
-        , h3 [] [ a [ href "mailto:contact@nickhollon.com" ] [ text "Get help today" ] ]
+        [ Markdown.toHtml content
         ]
+
+
+content : String
+content =
+    """
+
+# Nick Hollon solves problems.
+
+## Nick Hollon can solve _your_ problems.
+
+### [ Get help today. ](mailto:contact@nickhollon.com)
+"""
