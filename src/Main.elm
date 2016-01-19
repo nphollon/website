c545ca0e77e27cf6d92032e9bf8979269ed0c847
Nick Hollon <falling.masonry@gmail.com>
2016-01-19 10:49:59 -0800

    Make two-body orbital simulator

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,18 @@
 module Main (main) where
 
-import Html exposing (Html)
-import Effects exposing (Effects)
-import PhysicsFrame exposing (Model, init, update, view)
+import Html exposing (Html, main')
+import Markdown
+import Effects
+import PhysicsFrame as Phys
 import StartApp exposing (App)
 import Task exposing (Task)
 
 
-app : App Model
+app : App Phys.Model
 app =
     StartApp.start
-        { init = init
-        , update = update
+        { init = Phys.init
+        , update = Phys.update
         , view = view
         , inputs = []
         }
@@ -22,17 +23,24 @@ main =
     app.html
 
 
+view : Signal.Address Phys.Action -> Phys.Model -> Html
+view address model =
+    main'
+        []
+        [ intro
+        , Phys.view address model
+        ]
+
+
+intro : Html
+intro =
+    Markdown.toHtml """
+I am a software developer with a background in physics & astronomy.
+
+My latest project is a [classical mechanics library](http://package.elm-lang.org/packages/nphollon/mechanics/latest) for the [Elm programming language](http://elm-lang.org). Check here for updates and announcements!
+"""
+
+
 port tasks : Signal (Task Effects.Never ())
 port tasks =
     app.tasks
-
-
-content : String
-content =
-    """
-# Nick Hollon solves problems.
-
-## Nick Hollon can solve _your_ problems.
-
-### [ Get help today. ](mailto:contact@nickhollon.com)
-"""
--- src/Orbits.elm
+++ src/Orbits.elm
@@ -1,37 +1,141 @@
 module Orbits (Model, init, evolve, view) where
 
 import Graphics.Collage as Collage
+import Graphics.Element as Element exposing (Element)
 import Color
+import Dict exposing (Dict)
 import Html exposing (..)
 import Mechanics
 
 
 type alias Model =
-    Mechanics.State
+    StateWrapper
 
 
-init : Model
+type alias StateWrapper =
+    { state : Mechanics.State
+    , keys : Dict String { position : Int, size : Int }
+    }
+
+
+initialize : Dict String (List ( Float, Float )) -> StateWrapper
+initialize dataDict =
+    let
+        append key data { i, state, keys } =
+            let
+                size = List.length data
+            in
+                { i = i + size
+                , state = state ++ data
+                , keys = Dict.insert key { position = i, size = size } keys
+                }
+
+        flattened =
+            Dict.foldl append { i = 0, state = [], keys = Dict.empty } dataDict
+    in
+        { state = Mechanics.state 0 flattened.state
+        , keys = flattened.keys
+        }
+
+
+init : StateWrapper
 init =
-    Mechanics.state1 ( 0, -500 )
+    Debug.log "init"
+        <| initialize
+        <| Dict.fromList
+            [ ( "centerOfMass", [ ( 0, 0 ), ( 0, 0 ) ] )
+            , ( "planetA", [ ( 100, 0 ), ( degrees 0, 2 ) ] )
+            , ( "planetB", [ ( 100, 0 ), ( degrees 180, 2 ) ] )
+            ]
+
+
+wrap : Mechanics.State -> Dict String { position : Int, size : Int } -> StateWrapper
+wrap state keys =
+    { state = state
+    , keys = keys
+    }
+
+
+coordinate : String -> Int -> StateWrapper -> Float
+coordinate key index { state, keys } =
+    case Dict.get key keys of
+        Just { position, size } ->
+            if index < size then
+                Mechanics.coordinate (position + index) state
+            else
+                0
+
+        Nothing ->
+            0
+
+
+velocity : String -> Int -> StateWrapper -> Float
+velocity key index { state, keys } =
+    case Dict.get key keys of
+        Just { position, size } ->
+            if index < size then
+                Mechanics.velocity (position + index) state
+            else
+                0
+
+        Nothing ->
+            0
 
 
 evolve : Float -> Model -> Model
 evolve dt model =
     let
-        spring =
-            Mechanics.acceleration
-                <| \s -> [ -25 * (Mechanics.coordinate 0 s) ]
+        -- assumes two planets are of the same mass
+        gravity = 5000000.0
+
+        accel key s =
+            let
+                sw = { model | state = s }
+
+                radius = coordinate key 0 sw
+
+                radSpeed = velocity key 0 sw
+
+                rotSpeed = velocity key 1 sw
+            in
+                [ (radius * rotSpeed ^ 2) - (gravity * radius ^ -2)
+                , -2 * radSpeed * rotSpeed / radius
+                ]
+
+        totalAccel s =
+            [ 0, 0 ] ++ (accel "planetA" s) ++ (accel "planetB" s)
     in
-        Mechanics.evolve spring dt model
+        { model
+            | state =
+                Mechanics.evolve (Mechanics.acceleration totalAccel) dt model.state
+        }
 
 
 view : Model -> Html
 view model =
-    fromElement (Collage.collage 500 100 [ draw model ])
+    fromElement (draw model |> Element.color Color.white)
 
 
-draw : Mechanics.State -> Collage.Form
-draw state =
-    Collage.circle 20
-        |> Collage.filled Color.blue
-        |> Collage.moveX (Mechanics.coordinate 0 state)
+draw : Model -> Element
+draw model =
+    let
+        ball key =
+            let
+                radius = coordinate key 0 model
+
+                angle = coordinate key 1 model
+            in
+                Collage.circle 10
+                    |> Collage.filled Color.blue
+                    |> Collage.move (fromPolar ( radius, angle ))
+
+        origin =
+            Collage.circle 2 |> Collage.outlined Collage.defaultLine
+    in
+        Collage.collage
+            500
+            500
+            [ origin
+            , ball "planetA"
+            , ball "planetB"
+            ]
